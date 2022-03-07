a48a3b766ad12ea5a4509bdaf8cde02719eec709
Yara Elmhamid <yelmhamid@anrpcc.corp>
2022-03-07 10:02:19 +0100

    catching errors

--- src/Home.js
+++ src/Home.js
@@ -19,6 +19,7 @@ const Home = () => {
 
     const [blogs, setBlogs] = useState(null)  
     const [isPending,setIsPending] = useState(true)
+    const [error,setError] = useState(null)//want to output it to the browser=> store in state
     //const [name, setName] = useState('yara')
     /*const handleDelete = (id) => {
         //set new value of blogs to all except the id one deleted 
@@ -31,23 +32,36 @@ const Home = () => {
         //its asynchronis
         setTimeout(() => { //simulate a real get request that takes time
             fetch('http://localhost:8000/blogs') //return a promise
-        .then(res => {
-            return res.json() //its asynchronis
+                .then(res => {
+                    
+                    if(!res.ok){//check the reqponse object to see if there is any error
+                       //throw our own error
+                       throw Error('couldnt fetch the data for that ressource')
+                       
+                    }
+                    return res.json() //its asynchronis
 
-        }) //access promise
-        .then( (data) => {
-             setBlogs(data)
-             setIsPending(false)
-        })
+                }) //access promise
+                .then( (data) => {
+                    setBlogs(data)
+                    setIsPending(false)
+                    setError(null)
+                })
+                .catch(err =>
+                     {
+                        setError(err.message)
+                        setIsPending(false)
+                    })
         }, 1000);
         
 
     }, []);
-    //conditunal rending: 2nd thing will run only id 1st is true
+    //conditional rending: 2nd thing will run only id 1st is true
     return ( 
        <div className="home">
-            {isPending && <div>is loading...</div> }
-            { blogs && <Blog blogs ={blogs} title = "All blogs" />}
+           {error && <div>{error}</div> }
+           {isPending && <div>is loading...</div> }
+           {blogs && <Blog blogs ={blogs} title = "All blogs" />}
        </div>
      );
 }
