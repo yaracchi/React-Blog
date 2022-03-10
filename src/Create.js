f030f9feb8aa9d9a089fb9fd4b3d275135833652
Yara Elmhamid <yelmhamid@anrpcc.corp>
2022-03-10 15:32:38 +0100

    blog form creation. on change to update state

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,39 @@
+import { useState } from "react";
+
 const Create = () => {
+
+    const [title, setTitle] = useState('')
+    const [body, setBody] = useState('')
+    const [author, setAuthor] = useState('')
+
+
     return (  
         <div className="create"> 
             <h2> Add a new blog</h2>
+            <form >
+                <label>Blog title</label>
+                <input
+                 type="text"
+                 required
+                 value={title}
+                 onChange={(e) => setTitle(e.target.value)}/>
+
+                <label>Blog body</label>
+                <textarea required 
+                 value={body} 
+                 onChange={(e) => setBody(e.target.value)} />
+
+                <label>Blog author</label>
+                <select 
+                value = {author}
+                onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="yara">yara</option>
+                    <option value="lamia">lamia</option>
+                </select>
+
+                <button> Add blog</button>
+            </form>
         </div>
     );
 }
