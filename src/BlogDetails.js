594d86dfa6eb14ae85aeb8961dfbda0f92a38d93
Yara Elmhamid <yelmhamid@anrpcc.corp>
2022-03-14 15:25:55 +0100

    delete a blog using the fetch methode DELETE and redirect to home

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,19 +1,27 @@
+import { useHistory } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const { id } = useParams() //grap params from the route
     const {data :blog,isPending,error} = useFetch('http://localhost:8000/blogs/'+id)
-
+    const history = useHistory()
+    const handleClick = () => {
+        //fetch for the blog to delete from the json file
+        fetch('http://localhost:8000/blogs/'+ blog.id, { method: 'DELETE'} )
+        .then( () => { history.push('/') })
+    }
     return ( 
         <div className="blog-details" >
             {isPending && <div>Loading...</div> }
             {error && <div>{error} </div>}
             {blog && (
                 <article>
-                    <h2>{blog.title}</h2>   
-                    <p>{blog.body }</p>               
+                    <h2>{blog.title}</h2>                  
                     <p>written by{blog.author}</p>
+                    <div>{blog.body}</div>
+                    <button onClick={handleClick}>delete</button>
+
                 </article>
 
             )}
